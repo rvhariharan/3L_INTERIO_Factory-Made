e8ec64d3a148abb4da5c8e94a907d59950e4cf5b
rvhariharan <rvhariharan2004@gmail.com>
2026-05-08 14:58:52 +0530

    client work icon change

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MapPin, Quote, Star } from 'lucide-react';
+import { Briefcase, Quote, Star } from 'lucide-react';
 import { PARTNERS } from '../constants';
 
 const Partners: React.FC = () => {
@@ -45,16 +45,8 @@ const Partners: React.FC = () => {
                 <div className="flex flex-col space-y-2">
                   {partner.company && (
                     <div className="flex items-center text-gray-400 text-xs">
-                      <MapPin className="w-3 h-3 mr-2 text-secondary" />
+                      <Briefcase className="w-3 h-3 mr-2 text-secondary" />
                       {partner.company}
-                    </div>
-                  )}
-                  {partner.phone && (
-                    <div className="flex items-center text-gray-400 text-xs font-medium">
-                      <svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-3 mr-2 text-secondary" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-                      </svg>
-                      {partner.phone}
                     </div>
                   )}
                 </div>
